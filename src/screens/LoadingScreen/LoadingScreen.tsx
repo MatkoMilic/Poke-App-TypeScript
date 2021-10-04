aaa0ae4e2b8f4c8893e9a6bf4c468e35555b872f
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-10-04 11:37:04 +0200

    Add screens. Add styles.ts files. Add Index.ts files.

---
+++ src/screens/LoadingScreen/LoadingScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Loading: React.FC = () => {
+  return (
+    <View>
+      <Text>Loading Screen</Text>
+    </View>
+  );
+};
+
+export default Loading;
---
+++ src/screens/PokeListScreen/Index.ts
@@ -0,0 +1,1 @@
+export {default as PokeListScreen} from './PokeListScreen';
