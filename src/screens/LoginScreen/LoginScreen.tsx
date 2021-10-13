af674435d16ab09ef593eeb4a52fce3f1bad2c92
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-10-13 17:33:14 +0200

    Add other improvements to Login Screen.

--- src/screens/LoginScreen/LoginScreen.tsx
+++ src/screens/LoginScreen/LoginScreen.tsx
@@ -7,8 +7,12 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import {ScreenContainer} from '../../components';
 import {InputField} from '../../components/InputField';
 import {
+  MainStackParamList,
+  navigatorNames,
   OnboardingStackParamList,
   RootNavigatorParamsList,
+  screenNames,
+  UserValues,
 } from '../../constants';
 interface LoginProps {
   navigation: CompositeNavigationProp<
@@ -21,20 +25,23 @@ const Login: React.FC<LoginProps> = ({navigation}) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const signUpUser = async () => {
+  const signUpUser = () => {
     try {
-      const userDetails = {
+      const userDetails: UserValues = {
         email: email,
         password: password,
         favoritePokemon: 'null',
         theme: 'light',
       };
-      AsyncStorage.setItem(email, JSON.stringify(userDetails)).catch(() => {});
+      AsyncStorage.setItem(email, JSON.stringify(userDetails)).catch(error => {
+        console.log(error);
+      });
       AsyncStorage.setItem('activeUser', email);
       navigation.replace('MainNavigator', {screen: 'ProfileScreen'});
     } catch (err) {}
   };
 
+  //TODO: changes to the code of the next two functions:
   const validateInput = () => {
     const userInputEmpty = true
       ? email.length == 0 || password.length == 0
@@ -57,17 +64,20 @@ const Login: React.FC<LoginProps> = ({navigation}) => {
   const onSubmit = async () => {
     const doesUserExistAlready = await AsyncStorage.getItem(email);
     if (validateInput()) {
-      if (doesUserExistAlready != null) {
-        const fetchUser = await AsyncStorage.getItem(email);
-        const userParsed = fetchUser !== null ? JSON.parse(fetchUser) : null;
+      if (!doesUserExistAlready) {
+        signUpUser();
+        return;
+      } else if (doesUserExistAlready) {
+        const userParsed: UserValues = JSON.parse(doesUserExistAlready);
         if (userParsed.password != password) {
           Alert.alert('Warning', 'Entered password is not correct!');
         } else {
           AsyncStorage.setItem('activeUser', email);
-          navigation.replace('MainNavigator', {screen: 'ProfileScreen'});
+          navigation.replace(
+            navigatorNames.MAIN_NAVIGATOR as keyof RootNavigatorParamsList,
+            {screen: screenNames.PROFILE_SCREEN as keyof MainStackParamList},
+          );
         }
-      } else if (doesUserExistAlready == null) {
-        signUpUser();
       }
     }
   };
