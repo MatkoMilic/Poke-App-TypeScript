cfdf21122fe23822be984c5da78e983b0ce35fe8
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-10-14 13:21:39 +0200

    Add theme change function and button for it.

---
+++ src/components/ChangeTheme/ChangeTheme.tsx
@@ -0,0 +1,11 @@
+// const ChangeTheme = () => {
+//     const {toggleTheme} = React.useContext(ThemeContext);
+//     const userData = currentUserDetails;
+//     if (userData.theme == 'light') {
+//       userData.theme = 'dark';
+//     } else if (userData.theme == 'dark') {
+//       userData.theme = 'light';
+//     }
+//     AsyncStorage.setItem(currentUser, JSON.stringify(userData));
+//     return <Button title="Change Theme" onPress={() => toggleTheme()} />;
+//   };
