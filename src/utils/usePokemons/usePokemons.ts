8f5ef89ea465935a94a9bc50285530a393034166
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-10-20 15:26:25 +0200

    Add IUsePokemons interface.

--- src/utils/usePokemons/usePokemons.ts
+++ src/utils/usePokemons/usePokemons.ts
@@ -2,7 +2,13 @@ import {useCallback, useEffect, useRef, useState} from 'react';
 import {urls} from '../../constants';
 import {IPokemon} from '../../types';
 
-const usePokemons = () => {
+interface IusePokemons {
+  data: IPokemon[] | undefined;
+  error: null;
+  isLoading: boolean;
+}
+
+const usePokemons = (): IusePokemons => {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState<IPokemon[]>();
   const [error, setError] = useState(null);
